9336ae5fc2ea96aa30e826bd8d3dfa058149d5dd
Brian-Momata <brianmomata@gmail.com>
2023-04-30 04:27:31 +0300

    Combine #display_photos in #home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,22 +2,16 @@ require 'flickr'
 
 class StaticPagesController < ApplicationController
   def home
-    display_photos unless params[:flickr_id].nil?
-  end
-
-  private
-
-  def display_photos
-    flickr = Flickr.new
-    user_id = params[:flickr_id]
-    begin
-      @photos = flickr.people.getPublicPhotos(user_id: user_id)
-
-      puts @photos.inspect
-      
-    rescue StandardError => e
-      flash[:alert] = "Can't match the user ID"
-      redirect_to root_path
+    @photos = nil
+    unless params[:flickr_id].nil?
+      flickr = Flickr.new
+      user_id = params[:flickr_id]
+      begin
+        @photos = flickr.people.getPublicPhotos(user_id: user_id)
+      rescue StandardError => e
+        flash[:alert] = "Can't match the user ID"
+        redirect_to root_path
+      end
     end
   end
 end
